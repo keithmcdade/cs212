2746c52ab9592779f1d4cf816294b1fb8283ffeb
Keith McDade <kp.mac09@gmail.com>
2025-02-05 23:06:24 -0500

    first commit

---
+++ Lab_3/src/WriteName.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class WriteName {
+	public static void main(String args[]) throws FileNotFoundException {
+		
+		Scanner getName = new Scanner(System.in);
+		
+		System.out.print("Please enter your name: ");
+		String name = getName.next();
+		getName.close();
+		
+		File nameFile = new File("./name.txt");
+		if (nameFile.exists()) return;
+		
+		PrintWriter nameWriter = new PrintWriter(nameFile);
+		nameWriter.print(name);
+		nameWriter.close();
+		
+		Scanner readFile = new Scanner(nameFile);
+		System.out.println(readFile.next().toUpperCase());
+		readFile.close();
+	}
+}
