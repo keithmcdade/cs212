88dccbf03a39f5e3ced7a2b5ac3a04c695df8f1f
Keith McDade <kp.mac09@gmail.com>
2025-02-19 19:51:15 -0500

    first commit

---
+++ Lab_6/src/Lab6Main.java
@@ -0,0 +1,20 @@
+
+public class Lab6Main {
+    public static void main(String[] args) {
+        // Creating Student objects
+        Student student1 = new Student("Alice Johnson", "1234567890", 3.8);
+        Student student2 = new Student("Bob Smith", "6789012345", 3.2);
+	 
+        // Displaying student information
+        System.out.println(student1);
+        System.out.println();
+        System.out.println(student2);
+
+        System.out.println("student1 failed CS111...oops");
+        student1.setGPA(2.0);
+        System.out.println(student1);
+
+	
+    }
+}
+
---
+++ Lab_6/src/Student.java
@@ -0,0 +1,44 @@
+
+public class Student {
+	
+	private String name, id;
+	private Double gpa;
+	
+	// default constructor
+	public Student() {
+		name = null;
+		id = null;
+		gpa = null;
+	}
+	
+	// parameterized constructor
+	public Student(String name, String id, double gpa) {
+		this.name = name;
+		this.id = id;
+		this.gpa = gpa;
+	}
+	
+	// getters
+	public String getName() { return name; }
+	public String getID() { return id; }
+	public double getGPA() { return gpa; }
+	
+	// setters
+	public void setName(String name) { this.name = name; }
+	public void setID(String id) { this.id = id; }
+	public void setGPA(double gpa) { this.gpa = gpa; }
+
+	// returns true if student GPA > 3.5 else false
+	public boolean isOnHonorRoll() {
+		return (this.gpa >= 3.5);
+	}
+	
+	// overrides toString, returns student info as String
+	public String toString() {
+		return  "Name:   " + this.name + "\n" +
+				"ID:     " + this.id + "\n" +
+				"GPA:    " + this.gpa + "\n" +
+				"Honors: " + (this.isOnHonorRoll());
+	}
+	
+}
